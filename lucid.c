2d1cddf5278db3b9c955bb860143b6b640fb6090
Mike Tsao <mike@sowbug.com>
2012-04-09 21:04:16 -0700

    Code cleanup. Fix static initialization.

--- lucid.c
+++ lucid.c
@@ -24,6 +24,18 @@
 #define MACRO_WIDTH 1500
 #define MACRO_GAP 1500
 
+#if 1
+// Normal
+#define START_INTERRUPT_COUNT (57582)  // 3.5 * 60 * 60 * SLOW_HZ, 30 minutes
+#define MIN_INTERRUPT_COUNT (1096)  // 4 * 60 * SLOW_HZ, 4 minutes
+#define FLASH_COUNT (64)
+#else
+// Debugging
+#define START_INTERRUPT_COUNT (69)  // 15 seconds
+#define MIN_INTERRUPT_COUNT (14)  // 3 seconds
+#define FLASH_COUNT (4)
+#endif
+
 // Don't use this in real calculations unless you have room for the FP
 // library to be linked in
 #define SLOW_HZ (4.57)
@@ -32,78 +44,22 @@
 #define FALSE (!TRUE)
 
 enum {
-  MODE_WAITING,
-  MODE_DREAM
+  STATE_INIT,
+  STATE_WAITING,
+  STATE_DREAM
 };
-uint8_t mode;
 
 #define BUTTON PB4          // button, pin 3 to ground
 #define LED_LEFT _BV(PB3)   // pin 2
 #define LED_RIGHT _BV(PB2)  // pin 7
 #define LEDS (LED_LEFT | LED_RIGHT)
 
-volatile uint16_t interrupts_left;
-uint8_t flash_count;
-
 void set_fast_timer() {
-  TCCR0B = _BV(CS00); // new prescaler
+  TCCR0B = _BV(CS00);
 }
 
 static void set_slow_timer() {
-  TCCR0B = _BV(CS02) | _BV(CS00); // new prescaler
-}
-
-static void reset_fast_interrupts_left() {
-  interrupts_left = MACRO_WIDTH + MACRO_GAP;
-}
-
-static void switch_to_DREAM() {
-  mode = MODE_DREAM;
-  flash_count = 88;  // This is a guess.
-  set_fast_timer();
-  reset_fast_interrupts_left();
-}
-
-/*
- Assuming bedtime of 10:30pm, will flash on this schedule:
-
- 2:00:00 AM
- 3:45:00 AM
- 4:37:30 AM
- 5:03:45 AM
- 5:16:52 AM
- 5:23:26 AM
- 5:27:26 AM
- 5:31:26 AM
- 5:35:26 AM
- 5:39:26 AM
- 5:43:26 AM
- 5:47:26 AM
- 5:51:26 AM
- 5:55:26 AM
- 5:59:26 AM
- 6:03:26 AM
-
-*/
-#define START_INTERRUPT_COUNT (57582)  // 3.5 * 60 * 60 * SLOW_HZ, 30 minutes
-#define MIN_INTERRUPT_COUNT (1096)  // 4 * 60 * SLOW_HZ, 4 minutes
-static uint16_t interrupt_count = START_INTERRUPT_COUNT;
-static void reset_slow_interrupts_left() {
-  interrupts_left = interrupt_count;
-  interrupt_count = interrupt_count >> 1;
-  if (interrupt_count < MIN_INTERRUPT_COUNT) {
-    interrupt_count = MIN_INTERRUPT_COUNT;
-  }
-}
-
-static void set_slow_interrupts_to_min() {
-  interrupt_count = MIN_INTERRUPT_COUNT;
-}
-
-static void switch_to_WAITING() {
-  mode = MODE_WAITING;
-  set_slow_timer();
-  reset_slow_interrupts_left();
+  TCCR0B = _BV(CS02) | _BV(CS00);
 }
 
 static void leds_on() {
@@ -114,62 +70,148 @@ static void leds_off() {
   PORTB &= ~(LEDS);
 }
 
+/*
+  Assuming bedtime of 10:30pm, will flash on this schedule:
+
+  2:00:00 AM
+  3:45:00 AM
+  4:37:30 AM
+  5:03:45 AM
+  5:16:52 AM
+  5:23:26 AM
+  5:27:26 AM
+  5:31:26 AM
+  5:35:26 AM
+  5:39:26 AM
+  5:43:26 AM
+  5:47:26 AM
+  5:51:26 AM
+  5:55:26 AM
+  5:59:26 AM
+  6:03:26 AM
+
+*/
+static uint8_t state;
+static uint16_t interrupts_left;
+static uint8_t flash_count;
+static uint16_t interrupt_count;
+static uint8_t current_led;
+static void reset_waiting_interrupts_left() {
+  interrupts_left = interrupt_count;
+  interrupt_count = interrupt_count >> 1;
+  if (interrupt_count < MIN_INTERRUPT_COUNT) {
+    interrupt_count = MIN_INTERRUPT_COUNT;
+  }
+}
+
+static void reset_init_interrupts_left() {
+  interrupts_left = 9;  // about 2 seconds
+}
+
+static void switch_to_INIT() {
+  leds_on();
+  set_slow_timer();
+  state = STATE_INIT;
+  reset_init_interrupts_left();
+}
+
+static void set_waiting_interrupts_to_min() {
+  interrupt_count = MIN_INTERRUPT_COUNT;
+}
+
+static void switch_to_WAITING() {
+  leds_off();
+  set_slow_timer();
+  state = STATE_WAITING;
+  reset_waiting_interrupts_left();
+}
+
+static void reset_dream_interrupts_left() {
+  interrupts_left = MACRO_WIDTH + MACRO_GAP;
+}
+
+static void switch_to_DREAM() {
+  leds_off();
+  set_fast_timer();
+  state = STATE_DREAM;
+  flash_count = FLASH_COUNT;
+  current_led = LED_LEFT;
+  reset_dream_interrupts_left();
+}
+
 int is_button_pressed() {
   return !(PINB & _BV(BUTTON));
 }
 
 ISR(TIM0_OVF_vect) {
-  static uint8_t pwm;
-  static uint16_t transition;
-  static uint8_t current_led = LED_LEFT;
+  if (interrupts_left == 0) {
+    switch (state) {
+    case STATE_INIT:
+      if (is_button_pressed()) {
+        set_waiting_interrupts_to_min();
+        switch_to_DREAM();
+      } else {
+        switch_to_WAITING();
+      }
+      break;
 
-  if (interrupts_left != 0) {
+    case STATE_DREAM:
+      if (current_led == LED_LEFT) {
+        current_led = LED_RIGHT;
+      } else {
+        current_led = LED_LEFT;
+      }
+      reset_dream_interrupts_left();
+      if (--flash_count == 0) {
+        switch_to_WAITING();
+      }
+      break;
+
+    case STATE_WAITING:
+      switch_to_DREAM();
+      break;
+    }
+  } else {
     --interrupts_left;
-  }
 
-  switch (mode) {
-  case MODE_DREAM:
-    if (interrupts_left >= MACRO_WIDTH) {
-      pwm += PWM_VAL;
-      if (pwm > transition)
-        PORTB &= ~(current_led);
-      else
-        PORTB |= current_led;
+    switch (state) {
+    case STATE_INIT:
+      break;
 
-      if (!pwm)
-        transition += TRANS_VAL;
-    } else {
-      // delay between pulses
-      pwm = transition = 0;
-      leds_off();
-      if (interrupts_left == 0) {
-        if (current_led == LED_LEFT) {
-          current_led = LED_RIGHT;
-        } else {
-          current_led = LED_LEFT;
-        }
-        reset_fast_interrupts_left();
-        if (--flash_count == 0) {
-          switch_to_WAITING();
-        }
+    case STATE_DREAM: {
+      static uint8_t pwm;
+      static uint16_t transition;
+
+      if (interrupts_left >= MACRO_WIDTH) {
+        pwm += PWM_VAL;
+        if (pwm > transition)
+          PORTB &= ~(current_led);
+        else
+          PORTB |= current_led;
+
+        if (!pwm)
+          transition += TRANS_VAL;
+      } else {
+        pwm = transition = 0;
+        leds_off();
       }
     }
-    break;
+      break;
 
-  case MODE_WAITING:
-    if (interrupts_left == 0 || is_button_pressed())
-      switch_to_DREAM();
-    break;
+    case STATE_WAITING:
+      if (is_button_pressed())
+        switch_to_DREAM();
+      break;
+    }
   }
 }
 
 // init the IRQ
-void irqinit (void) {
+void init_irq (void) {
   set_slow_timer();
 
   // Enable timer overflow irq
   TIMSK0 = _BV(TOIE0);
-  sei(); // IRQ on
 }
 
 int main(void) {
@@ -178,29 +220,17 @@ int main(void) {
 
   DDRB |= _BV(DDB3) | _BV(DDB2); // set output
 
-  // init the IRQ
-  irqinit();
+  init_irq();
 
-  // delay before checking user input again
-  leds_on();
-  interrupts_left = 9; // SLOW_HZ * 2, about 2 seconds
-  while (interrupts_left > 0) {
-  }
-  leds_off();
+  interrupt_count = START_INTERRUPT_COUNT;
+  switch_to_INIT();
+  sei();
 
-  if (is_button_pressed()) {
-    set_slow_interrupts_to_min();
-    switch_to_DREAM();
-  } else {
-    switch_to_WAITING();
-  }
-
-  // place the CPU into idle mode
   set_sleep_mode(SLEEP_MODE_IDLE);
   sleep_mode();
 
-  // infinite loop--the IRQ does all the work...
   while (TRUE) {
+    // infinite loop--the IRQ does all the work...
   }
 
   return 0;
